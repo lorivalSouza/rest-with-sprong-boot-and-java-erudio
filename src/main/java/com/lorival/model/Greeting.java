eff08c7e1e7219833da1346de7b537e321f3858e
lorivalSouza <lorivalnew@gmail.com>
2026-06-20 19:17:52 -0300

    First steps in Java with Spring Boot

---
+++ src/main/java/com/lorival/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.lorival.model;
+
+public record Greeting(long id, String content) {}
---
+++ src/test/java/com/lorival/StartupTests.java
@@ -0,0 +1,13 @@
+package com.lorival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
