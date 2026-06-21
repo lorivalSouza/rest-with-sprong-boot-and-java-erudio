505daa3e02f229f9d5a2b3d22f56a8f3ffebe524
lorivalSouza <lorivalnew@gmail.com>
2026-06-21 13:44:41 -0300

    Endpoint math SUM

--- src/main/java/com/lorival/Startup.java
+++ src/main/java/com/lorival/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Statup {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Statup.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
